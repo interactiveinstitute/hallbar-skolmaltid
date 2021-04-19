1a8cc8b3079f969269745c8735e35da0b95172dd
Gunnar <gunnar.oledal@gmail.com>
2021-04-19 11:28:38 +0000

    script for performing mongodump

---
+++ server/scripts/backup-mongo.sh
@@ -0,0 +1,14 @@
+# !/bin/sh
+
+DATE=$(date "+%Y%m%d-%H_%M_%S")
+
+
+# docker exec -it db-mongo mongodump --host localhost --port 27017 -o 
+# docker exec -it db-mongo tar -zcvf $current_date.tar.gz dump
+# docker cp db-mongo:/$current_date.tar.gz ~/mongodumps/
+
+cd ../docker/
+
+
+echo "Gonna backup to file: ~/mongodumps/$DATE.dump"
+docker-compose exec -T mongo-db sh -c 'mongodump --archive' >  ~/mongodumps/$DATE.dump
